fa1beafe8c1e0bcc2e3c146735d3fdc625bd22c1
Ayaan Ahmed Abbasi <ayaanah776@gmail.com>
2026-04-25 14:13:38 +0530

    minor changes

--- backend/validation/authvalidation.js
+++ backend/validation/authvalidation.js
@@ -4,7 +4,7 @@ const Joi=require("joi");
 
 const signupSchemaValid = Joi.object({
   username: Joi.string().min(3).max(30).required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().required(),
   email: Joi.string().email().required()
 });
 
